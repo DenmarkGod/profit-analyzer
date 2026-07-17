92b6de57df5cad9f0eb0e14151561f764c06b5de
DenmarkGod <145672238+DenmarkGod@users.noreply.github.com>
2026-07-17 18:32:50 +0300

    Delete js directory

--- js/config.js
+++
@@ -1,6 +0,0 @@
-const CONFIG = {
-    PROXY_URL: 'https://moysklad-proxy.helsinkyofficial.workers.dev',
-    CACHE_VERSION: 'v2', // Увеличиваем версию, чтобы сбросить старый некорректный кэш
-    RETRIES: 3,
-    RETRY_DELAY: 2000
-};
